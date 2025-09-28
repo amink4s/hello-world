3e863094ddaee2a79cc00125325577631695c971
Mikadoe <170118553+amink4s@users.noreply.github.com>
2025-09-28 14:47:06 +0000

    added main.py which is the program that calls list_module.py

---
+++ Python/exam/main.py
@@ -0,0 +1,8 @@
+from list_module import addition, power_of_list
+
+test_list = [1, 2, 3, 4, 5]
+add_number = 10
+power_number = 3
+
+print(addition(test_list, add_number))
+print(power_of_list(test_list, power_number))
